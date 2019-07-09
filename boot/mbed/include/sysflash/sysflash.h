576b267db8d7ec039ae1940002629099a1b14bd8
George Beckstein <donbeckstein@live.com>
2019-07-09 19:59:13 -0400

    Filled out flash algorithm and headers needed by mcuboot

---
+++ boot/mbed/include/sysflash/sysflash.h
@@ -0,0 +1,15 @@
+/* Manual version of auto-generated version. */
+
+#ifndef __SYSFLASH_H__
+#define __SYSFLASH_H__
+
+#define RESERVED_ID		0
+#define PRIMARY_ID		1
+#define SECONDARY_ID		2
+#define SCRATCH_ID		3
+
+#define FLASH_AREA_IMAGE_PRIMARY    PRIMARY_ID
+#define FLASH_AREA_IMAGE_SECONDARY  SECONDARY_ID
+#define FLASH_AREA_IMAGE_SCRATCH    SCRATCH_ID
+
+#endif /* __SYSFLASH_H__ */
